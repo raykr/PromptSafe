4b34ba7e6d00849d96843e4b428bd5adfca2cf7e
raykr <raykr@buaa.edu.cn>
2026-02-06 11:07:04 +0800

    sd3 check

--- sd3/check_sd35.sh
+++ sd3/check_sd35.sh
@@ -1,39 +1,39 @@
-# CUDA_VISIBLE_DEVICES=1 python checkers/ud_checker.py --images_dir results/multi_encoder/sd35_emb_all_sexual_schemeB/adapter_all
-# CUDA_VISIBLE_DEVICES=1 python checkers/ud_checker.py --images_dir results/multi_encoder/sd35_emb_all_sexual_schemeB/baseline
-# python metric.py "results/multi_encoder/sd35_emb_all_sexual_schemeB/*" --output results/multi_encoder/sd35_emb_all_sexual_schemeB/results.txt
+# CUDA_VISIBLE_DEVICES=1 python checkers/ud_checker.py --images_dir results/sd3/sd35_sexual/adapter_all
+# CUDA_VISIBLE_DEVICES=1 python checkers/ud_checker.py --images_dir results/sd3/sd35_sexual/baseline
+# python metric.py "results/sd3/sd35_sexual/*" --output results/sd3/sd35_sexual/results.txt
 
 
-# CUDA_VISIBLE_DEVICES=1 python checkers/ud_checker.py --images_dir results/multi_encoder/sd35_emb_all_violent_schemeB/adapter_all
-# CUDA_VISIBLE_DEVICES=1 python checkers/ud_checker.py --images_dir results/multi_encoder/sd35_emb_all_violent_schemeB/baseline
-# python metric.py "results/multi_encoder/sd35_emb_all_violent_schemeB/*" --output results/multi_encoder/sd35_emb_all_violent_schemeB/results.txt
+# CUDA_VISIBLE_DEVICES=1 python checkers/ud_checker.py --images_dir results/sd3/sd35_violent/adapter_all
+# CUDA_VISIBLE_DEVICES=1 python checkers/ud_checker.py --images_dir results/sd3/sd35_violent/baseline
+# python metric.py "results/sd3/sd35_violent/*" --output results/sd3/sd35_violent/results.txt
 
 
-# CUDA_VISIBLE_DEVICES=1 python checkers/ud_checker.py --images_dir results/multi_encoder/sd35_emb_all_political_schemeB/adapter_all
-# CUDA_VISIBLE_DEVICES=1 python checkers/ud_checker.py --images_dir results/multi_encoder/sd35_emb_all_political_schemeB/baseline
-# python metric.py "results/multi_encoder/sd35_emb_all_political_schemeB/*" --output results/multi_encoder/sd35_emb_all_political_schemeB/results.txt
+# CUDA_VISIBLE_DEVICES=1 python checkers/ud_checker.py --images_dir results/sd3/sd35_political/adapter_all
+# CUDA_VISIBLE_DEVICES=1 python checkers/ud_checker.py --images_dir results/sd3/sd35_political/baseline
+# python metric.py "results/sd3/sd35_political/*" --output results/sd3/sd35_political/results.txt
 
 
-# CUDA_VISIBLE_DEVICES=1 python checkers/ud_checker.py --images_dir results/multi_encoder/sd35_emb_all_disturbing_schemeB/adapter_all
-# CUDA_VISIBLE_DEVICES=1 python checkers/ud_checker.py --images_dir results/multi_encoder/sd35_emb_all_disturbing_schemeB/baseline
-# python metric.py "results/multi_encoder/sd35_emb_all_disturbing_schemeB/*" --output results/multi_encoder/sd35_emb_all_disturbing_schemeB/results.txt
+# CUDA_VISIBLE_DEVICES=1 python checkers/ud_checker.py --images_dir results/sd3/sd35_disturbing/adapter_all
+# CUDA_VISIBLE_DEVICES=1 python checkers/ud_checker.py --images_dir results/sd3/sd35_disturbing/baseline
+# python metric.py "results/sd3/sd35_disturbing/*" --output results/sd3/sd35_disturbing/results.txt
 
 
 CUDA_VISIBLE_DEVICES=1 python checkers/compute_clip.py \
     --csv_path datasets/test/test_benign_200.csv \
-    --image_dir results/multi_encoder/sd35_emb_all_benign_schemeB/adapter_all \
-    --output results/multi_encoder/sd35_emb_all_benign_schemeB/clip_adapter_all.json
+    --image_dir results/sd3/sd35_benign/adapter_all \
+    --output results/sd3/sd35_benign/clip_adapter_all.json
 CUDA_VISIBLE_DEVICES=1 python checkers/compute_lpips.py \
-    --gen_dir results/multi_encoder/sd35_emb_all_benign_schemeB/adapter_all \
+    --gen_dir results/sd3/sd35_benign/adapter_all \
     --ori_dir datasets/test/test_benign_200 \
-    --output results/multi_encoder/sd35_emb_all_benign_schemeB/lpips_adapter_all.json
+    --output results/sd3/sd35_benign/lpips_adapter_all.json
 
 CUDA_VISIBLE_DEVICES=1 python checkers/compute_clip.py \
     --csv_path datasets/test/test_benign_200.csv \
-    --image_dir results/multi_encoder/sd35_emb_all_benign_schemeB/baseline \
-    --output results/multi_encoder/sd35_emb_all_benign_schemeB/clip_baseline.json
+    --image_dir results/sd3/sd35_benign/baseline \
+    --output results/sd3/sd35_benign/clip_baseline.json
 CUDA_VISIBLE_DEVICES=1 python checkers/compute_lpips.py \
-    --gen_dir results/multi_encoder/sd35_emb_all_benign_schemeB/baseline \
+    --gen_dir results/sd3/sd35_benign/baseline \
     --ori_dir datasets/test/test_benign_200 \
-    --output results/multi_encoder/sd35_emb_all_benign_schemeB/lpips_baseline.json
+    --output results/sd3/sd35_benign/lpips_baseline.json
 
-python metric.py "results/multi_encoder/sd35_emb_all_benign_schemeB/*" --output results/multi_encoder/sd35_emb_all_benign_schemeB/results.txt
+python metric.py "results/sd3/sd35_benign/*" --output results/sd3/sd35_benign/results.txt
